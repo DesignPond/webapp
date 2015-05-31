8ad599985b1a2cc2fe8e9bb6673e76e2437bce88
Cindy Leschaud <cindy.leschaud@gmail.com>
2015-05-31 11:43:47 +0200

    traduction in english

---
+++ resources/lang/en/action.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Empty messages Language
+    |--------------------------------------------------------------------------
+    */
+
+    'maj'            => 'Update!',
+    'partager'       => 'Share!',
+    'seeall'         => 'See all',
+    'seealert'       => 'See alerts',
+    'save'           => 'Save',
+    'change'         => 'Change',
+    'login'          => 'Login',
+    'back'           => 'Back to list',
+    'backto'         => 'Back',
+    'ok'             => 'Ok',
+    'ou'             => 'Or',
+    'oui'            => 'Yes',
+    'non'            => 'No',
+    'destroy'        => 'Destroy sharing',
+    'delete'         => 'Delete',
+    'search'         => 'Search',
+    'create'         => 'Create account',
+    'select'         => 'Select image',
+    'send'           => 'Send a sharing request',
+    'ipartage'       => 'I share the following informations',
+    'iwant'          => 'I would the following like information',
+    'moreactivites'  => 'See more activities',
+];
---
+++ resources/lang/en/empty.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Empty messages Language
+    |--------------------------------------------------------------------------
+    */
+
+    'contact'   => 'You don\'t have any contacts yet.',
+    'activites' => 'No activity yet',
+    'noinfo'    => 'you haven\'t entered any of your informations',
+];
