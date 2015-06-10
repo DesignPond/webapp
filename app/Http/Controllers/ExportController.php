1d4550a856ae2780aa5b7e4d91ec03a0604c7c79
Cindy Leschaud <cindy.leschaud@gmail.com>
2015-06-10 18:56:55 +0200

    change name of exported file

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -64,7 +64,7 @@ class ExportController extends Controller {
         $types = $this->export->prepareLabelsTitle();
         $lines = $this->export->userExport();
         
-        \Excel::create('Filename', function($excel) use ($lines,$types) {
+        \Excel::create('Exportation', function($excel) use ($lines,$types) {
 
             $excel->sheet('Export', function($sheet) use ($lines,$types) {
                 $sheet->fromArray($lines);
